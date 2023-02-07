806fb7ba575d296a69549ea779b5d3f4793ab8c7
ChouL1n <33456604+ChouL1n@users.noreply.github.com>
2023-02-07 17:09:53 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      todos: [],
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleAddTodo = () => {
+    const newTodo = {
+      text: this.state.inputValue,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+      inputValue: "",
+    });
+  };
+
+  handleDeleteTodo = (index) => {
+    const newTodos = [...this.state.todos];
+    newTodos.splice(index, 1);
+    this.setState({ todos: newTodos });
+  };
+
+  render() {
+    return (
+      <div className="todo-list">
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+          placeHolder="新增待辦事項"
+        />
+        <button onClick={this.handleAddTodo} className="add-btn">新增</button>
+        <ul>
+          {this.state.todos.map((todo, index) => (
+            <li key={index}>
+              {todo.text}
+              <button onClick={() => this.handleDeleteTodo(index)}>
+                ✔️
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoList;
